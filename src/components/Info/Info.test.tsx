8491e2d9d2101535b714455f629670483bcff2bd
Conchi Carcedo <concepcion.carnero@docplanner.com>
2023-04-26 23:13:28 +0200

    Add info component with test

---
+++ src/components/Info/Info.test.tsx
@@ -0,0 +1,16 @@
+import { render, screen } from "@testing-library/react";
+import Info from "./Info";
+
+describe("Given an Info component", () => {
+  describe("When rendered", () => {
+    test("Then it should show the text 'Calling...", () => {
+      const expectedText = "Calling...";
+
+      render(<Info />);
+
+      const text = screen.getByText(expectedText);
+
+      expect(text).toBeInTheDocument();
+    });
+  });
+});
