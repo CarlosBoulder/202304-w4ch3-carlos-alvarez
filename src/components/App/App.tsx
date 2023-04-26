855f0b6cbde884cc86a0184eb58530a350df1152
Conchi Carcedo <concepcion.carnero@docplanner.com>
2023-04-27 00:05:54 +0200

    Add keyboard component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,9 +1,11 @@
 import Info from "../Info/Info";
+import Keyboard from "../Keyboard/Keyboard";
 
 const App = (): JSX.Element => {
   return (
     <div className="container">
       <Info />
+      <Keyboard />
     </div>
   );
 };
